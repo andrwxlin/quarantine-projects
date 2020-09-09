73c494cf87c265255b121d3bcf1b7aeac3650afd
andrwxlin <asyasylin@gmail.com>
2020-09-09 12:37:23 -0500

    looping

--- python/zoom.py
+++ python/zoom.py
@@ -2,17 +2,22 @@ from pypresence import Presence
 import time
 import datetime
 
-asdf = input("What class?")
-
 client_id = "753283872443203636"
 RPC = Presence(client_id)
 RPC.connect()
 
-try:
+def meeting_RPC():
+    asdf = input("What class?")
     start_time = time.time()
-    while True:
-        RPC.update(details='In a meeting', state=asdf, large_image="zoom", start=start_time)
-        time.sleep(60)
+    RPC.update(details='In a meeting', state=asdf, large_image="zoom", start=start_time)
+    decision = input("CTRL+C or close this window to exit, press ENTER to start a new meeting")
+    if decision == "":
+        meeting_RPC()
+    else:
+        print("Invalid response")
+
+try:
+    meeting_RPC()
 except KeyboardInterrupt:
     print("class ended")
     RPC.close()
