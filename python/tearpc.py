b42252e168d91b8c9cfcc8f3de35d7aa7287638f
andrwxlin <asyasylin@gmail.com>
2020-10-02 13:51:40 -0500

    good stuff

--- python/tearpc.py
+++ python/tearpc.py
@@ -9,9 +9,8 @@ tea = input("what tea?")
 print(images)
 limage = input("large image?")
 print(toppings)
-toppings = input("what additions?")
+topping = input("what additions?")
 simage = input("small image?")
-
 ltt = input("large tooltip?")
 stt = input("small tooltip?")
 
@@ -23,15 +22,26 @@ aaaa = time.time()
 
 count = 1
 def tea_update():
-    RPC.update(details = tea + " with " + toppings, state = "cup #" + str(count), large_image = limage, small_image = simage, large_text = ltt, small_text = stt, start=aaaa)
+    RPC.update(details = tea + " with " + topping, state = "cup #" + str(count), large_image = limage, small_image = simage, large_text = ltt, small_text = stt, start=aaaa)
 
 try:
     while True:
         tea_update()
-        counter = input("press enter to add 1 cup")
+        counter = input("press enter to add 1 cup, \'update\' to update your rich presence stuff, or \'reset\' to reset the number of cups")
         if counter == "":
             count += 1
             tea_update()
+        elif counter == "update":
+            tea = input("what tea?")
+            print(images)
+            limage = input("large image?")
+            print(toppings)
+            topping = input("what additions?")
+            simage = input("small image?")
+            ltt = input("large tooltip?")
+            stt = input("small tooltip?")
+        elif counter == "reset":
+            count = 1
         else:
             print("invalid response")
 except KeyboardInterrupt:
